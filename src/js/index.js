69cdf7356d5f66a3b5dd98521f7fea133759effd
Danilo de Canini <danilodecanini@gmail.com>
2019-06-13 23:49:27 -0300

    adding more features like the recipe view render

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app 
@@ -16,7 +17,8 @@ const state = {};
  */
 const controlSearch = async () => {
     // 1) Get Query from view
-    const query = searchView.getInput(); //TODO
+    //const query = searchView.getInput();
+    const query = 'pizza';
 
     if(query) {
         // 2) New search object and add to state
@@ -27,16 +29,29 @@ const controlSearch = async () => {
         searchView.clearResults();
         renderLoader(elements.searchRes);
 
-        // 4) Search for recipes
-        await state.search.getResults();
+        try {
+            // 4) Search for recipes
+            await state.search.getResults();
 
-        // 5) Render results on UI
-        clearLoader();
-        searchView.renderResults(state.search.result);
+            // 5) Render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);
+
+        } catch (error) {
+            alert('Something wrong with the search...');
+            clearLoader();
+        }
+        
     }
 }
 
 elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+//Testing
+window.addEventListener('load', e => {
     e.preventDefault();
     controlSearch();
 });
@@ -56,6 +71,62 @@ elements.searchResPages.addEventListener('click', e => {
  * 
  */
 
- const r = new Recipe(35477);
- r.getRecipe();
- console.log(r);
+ const controlRecipe = async () => {
+     //Get ID from url
+     const id = window.location.hash.replace('#', '');
+     console.log(id);
+
+     if (id) {
+        // Prepare UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
+
+        // Highlight selected search item
+        if (state.search) searchView.highlightSelected(id);
+
+         // Create new recipe obect
+        state.recipe = new Recipe(id);
+
+        //TESTING
+        window.r = state.recipe;
+
+        try {
+             // Get recipe data
+            await state.recipe.getRecipe();
+            state.recipe.parseIngredients();
+
+            // Calculate serving and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+
+            // Render Recipe
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
+            
+        } catch (error) {
+            alert(error);
+        }
+        
+     }
+ }
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+
+//Handling recipe button clicks
+
+elements.recipe.addEventListener('click', e => {
+    if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+        // Decrease button is clicked
+        if (state.recipe.servings > 1) {
+            state.recipe.updateServings('dec');
+            recipeView.updateServingsIngredients(state.recipe);
+        }
+        
+    } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+        // Increase button is clicked
+        state.recipe.updateServings('inc');
+        recipeView.updateServingsIngredients(state.recipe);
+    }
+
+});
